81ffe3754dbed1ed00a8a986b2dbe6af731ec145
dairof7 <dairof7@gmail.com>
2020-04-06 21:58:11 -0500

    functions for strings

---
+++ strfunctions.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: char array pointer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
