72f3e33d7e3027ca20928b728243a96020c32d2d
untalinfo <racso1607@gmail.com>
2020-04-07 21:26:49 +0000

    aregada la descripcion y free a data

--- messages.c
+++ messages.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
 
+/**
+ * end_of_file - function that deteted EOF and exit the process
+ * data: data of getline
+ * Return: none
+ */
 void end_of_file(char *data)
 {
 
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, "\n", 1);
-//	free(data);
+	free(data);
 	exit(0);
 }
