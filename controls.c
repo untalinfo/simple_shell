aa6d0ee5d4e1a84d67bce45b77e290b345c54d9d
untalinfo <racso1607@gmail.com>
2020-04-08 22:41:33 +0000

    en esta funcion estan las funciones de ctrl c y ctrl d

---
+++ controls.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_prompt - function that prints the prompt
+ * 
+ * Return: None
+ */
+void print_prompt(void)
+{
+        if (isatty(STDIN_FILENO))
+        	write(STDOUT_FILENO, "$ ", 2);
+}
+/**
+ * ctrlC - function that handles signal and write the prompt
+ * @sig: signal to handler
+ * Return: None
+ */
+void ctrlC(int sig)
+{
+        (void) sig;
+        write(STDOUT_FILENO, "\n$ ", 3);
+}
+/**
+ * end_of_file - function that deteted EOF and exit the process
+ * @data: data of getline
+ * Return: none
+ */
+void end_of_file(char *data)
+{
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+	free(data);
+	exit(0);
+}
