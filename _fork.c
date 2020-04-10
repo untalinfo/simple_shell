db6a1b0b35747e42d3d9278c567b090a581119ae
dairof7 <dairof7@gmail.com>
2020-04-10 14:51:52 -0500

    move fork() and condicionals from prompt to _fork function, 0 leaks 0 error

--- _fork.c
+++ _fork.c
@@ -10,17 +10,29 @@ int _fork(char **token, const list_t *head)
 
 	int i = 0;
 	struct stat st;
+	pid_t hijo;
+	int w = 0;
 
-	if (stat(token[i], &st) == 0)
-	{
-		if (execve(token[0], token, NULL) == -1)
-		{
-			perror("Error: ");
-			return (-1);
-		}
-	}
-	else
-		search_path(token, head);
+	hijo = fork();
+
+			if (hijo < 0)
+				_fork_fail();
+			if (hijo == 0)
+			{
+				if (stat(token[i], &st) == 0)
+				{
+					if (execve(token[0], token, NULL) == -1)
+					{
+						perror("Error: ");
+						return (-1);
+					}
+				}
+				else
+					search_path(token, head);
+				exit(-1);
+			}
+			else
+				wait(&w);
 	return (1);
 }
 /**
