7918ba120095d9a4643585b9c7a6add30f78609c
dairof7 <dairof7@gmail.com>
2020-04-15 23:15:52 -0500

    remove the mod_pwd function

--- functions.c
+++ functions.c
@@ -3,16 +3,14 @@
  * _cd - function that allows to change directory
  * @token: pointer to commands from tok
  * @current: pointer to current directory
- * @env: enviroment variable
+ * @env: enviroment
  * Return: current
  */
 char *_cd(char **token, char *current, char **env)
 {
-	/*size_t len = 200;*/
 	char *buff = NULL, *home = NULL;
 
 	buff = malloc(200);
-	/*getcwd(p, len);*/
 	if (buff == NULL)
 		return (NULL);
 	getcwd(buff, 200);
@@ -22,14 +20,12 @@ char *_cd(char **token, char *current, char **env)
 		home = _getenv("HOME", env);
 		chdir(home);
 		getcwd(current, 200);
-		mod_pwd(current, env);
 	}
 	else if (_strcmp(token[1], "-") == 0)
 	{
 		write(1, current, _strlen(current));
 		write(1, "\n", 1);
 		chdir(current);
-		mod_pwd(current, env);
 	}
 	else
 	{
